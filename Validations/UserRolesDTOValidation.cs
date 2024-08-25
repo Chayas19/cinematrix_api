272a1ace270c48ee4133edd804ffab0be8fcd58f
Chayas19 <chaya@nproglobal.com>
2024-08-25 16:32:39 +0530

    added validations for the userroles dto

---
+++ Validations/UserRolesDTOValidation.cs
@@ -0,0 +1,19 @@
+﻿using CineMatrix_API.DTOs;
+using CineMatrix_API.Enums;
+using FluentValidation;
+
+namespace CineMatrix_API.Validations
+{
+    public class UserRolesDTOValidation : AbstractValidator<UserRolesDTO>
+    {
+        public UserRolesDTOValidation()
+        {
+            RuleFor(dto => dto.UserId)
+                    .GreaterThan(0).WithMessage("UserId must be greater than 0.");
+
+            RuleFor(dto => dto.RoleName)
+                    .NotEmpty().WithMessage("RoleName is required.")
+                    .Must(roleName => Enum.IsDefined(typeof(RoleType), roleName)).WithMessage("Invalid RoleName specified.");
+        }
+    }
+}
